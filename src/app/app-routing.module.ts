724ec924adfd9a9cc81acc53e3dc7e8e0f39cb95
Elton Claus <eltonclaus2@gmail.com>
2023-11-02 21:04:58 -0300

    Components createds with styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'blog',
+    component:BlogComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { NewsCardComponent } from './components/news-card/news-card.component';
+import { VideoCardComponent } from './components/video-card/video-card.component';
+import { FlashCardComponent } from './components/flash-card/flash-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    NewsCardComponent,
+    VideoCardComponent,
+    FlashCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
